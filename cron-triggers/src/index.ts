0ae7c1544508fc7be7bc588897f3ef2fcf35ee05
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-07-15 23:40:45 +0900

    add sample cron-trigger event

--- cron-triggers/src/index.ts
+++ cron-triggers/src/index.ts
@@ -1,9 +1,34 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
 
-const app = new Hono()
+type Env = {
+  WEBHOOK_URL: string;
+}
+
+type App = {
+  Env: Env;
+  Bindings: CloudflareBindings;
+}
+
+const app = new Hono<App>();
 
 app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.text('Hello Hono!');
+});
 
-export default app
+const handler: ExportedHandler<Env> = {
+  fetch: app.fetch,
+  scheduled: async (controller, env, ctx) => {
+    const r = await fetch(env.WEBHOOK_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        content: `Scheduled event triggered!: ${controller.cron}`,
+      }),
+    })
+    console.log(`Webhook response: ${r.status} ${r.statusText}`);
+  }
+}
+
+export default handler;
