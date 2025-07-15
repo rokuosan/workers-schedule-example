ad8a7b3f04f7eac33185bacfc829506060dd33fd
Koki Matsumoto <85651386+rokuosan@users.noreply.github.com>
2025-07-16 01:12:21 +0900

    feat: sample scheduler

--- durable-objects/src/index.ts
+++ durable-objects/src/index.ts
@@ -1,9 +1,53 @@
-import { Hono } from 'hono'
+import { DurableObject } from 'cloudflare:workers';
+import { Hono } from 'hono';
+import { hc } from 'hono/client';
 
-const app = new Hono()
+type Env = {
+  WEBHOOK_URL: string;
+};
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+type App = {
+  Env: Env;
+  Bindings: CloudflareBindings;
+};
 
-export default app
+const app = new Hono<App>().post('/alarm', async (c) => {
+  const r = await fetch(c.env.WEBHOOK_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      content: `Alarm triggered at ${new Date().toISOString()}`,
+    }),
+  });
+  return c.json(r.json());
+});
+
+const client = hc<typeof app>('/');
+
+export class Scheduler extends DurableObject<Env> {
+  storage: DurableObjectStorage;
+
+  constructor(ctx: DurableObjectState, env: Env) {
+    super(ctx, env);
+    this.storage = ctx.storage;
+  }
+
+  async alarm(alarmInfo?: AlarmInvocationInfo): Promise<void> {
+    if (alarmInfo?.isRetry) return;
+
+    console.log(`Alarm triggered at ${new Date().toISOString()}`);
+    const alarm = await this.storage.getAlarm({ allowConcurrency: false });
+    console.log(
+      `Current alarm: ${alarm ? new Date(alarm).toISOString() : 'none'}`,
+    );
+
+    const r = await client.alarm.$post();
+    console.log(`Webhook response: ${r.status} ${r.statusText}`);
+
+    this.storage.setAlarm(Date.now() + 2 * 60 * 1000); // Set next alarm for 2 minutes later
+  }
+}
+
+export default app;
